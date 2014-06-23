95550ad878104fc47d5992a8333f6c9da3bfa820
Esteban Uribe <estebanuribe@gmail.com>
2014-06-22 20:10:48 -0700

    Tweaked for iOS 7

--- Lorenz/LorenzView.m
+++ Lorenz/LorenzView.m
@@ -68,8 +68,8 @@ void drawLeadPointInContext(double xd, double yd, CGContextRef context, CGColor 
     if (self = [super initWithCoder:aDecoder]) {
         savedDrawingImage = nil;
         x = 0.001, y = 0, z = 0;
-        x2 = 0.11, y2 = 0, z2 = 0;
-        x3 = 0.101, y3 = 0, z3 = 0;
+//        x2 = 0.11, y2 = 0, z2 = 0;
+//        x3 = 0.101, y3 = 0, z3 = 0;
         red = 0, green = 0, blue = 0;
         iterator = 0;
         
@@ -86,10 +86,10 @@ void drawLeadPointInContext(double xd, double yd, CGContextRef context, CGColor 
         
         old_xd = 0 + self.center.x;
         old_yd = 0 + self.center.y;
-        old_x2d = 0 + self.center.x;
-        old_y2d = 0 + self.center.y;
-        old_x3d = 0 + self.center.x;
-        old_y3d = 0 + self.center.y;
+//        old_x2d = 0 + self.center.x;
+//        old_y2d = 0 + self.center.y;
+//        old_x3d = 0 + self.center.x;
+//        old_y3d = 0 + self.center.y;
         
         _pause = NO;
         _clear = NO;
@@ -119,8 +119,8 @@ void drawLeadPointInContext(double xd, double yd, CGContextRef context, CGColor 
 {
     if (_restart) {
         x = 0.0000000001, y = 0, z = 0;
-        x2 = 0.11, y2 = 0, z2 = 0;
-        x3 = 0.101, y3 = 0, z3 = 0;
+//        x2 = 0.11, y2 = 0, z2 = 0;
+//        x3 = 0.101, y3 = 0, z3 = 0;
         iterator = 0;
         _clear = YES;
         _restart = NO;
@@ -153,8 +153,8 @@ void drawLeadPointInContext(double xd, double yd, CGContextRef context, CGColor 
     NSAssert(context != NULL, @"Context is NULL!");
 
     if(isnan(x) || isnan(y) || isnan(z)) x = 0.1, y = 0, z = 0;
-    if(isnan(x2) || isnan(y2) || isnan(z2)) x2 = 0.11, y2 = 0, z2 = 0;
-    if(isnan(x3) || isnan(y3) || isnan(z3)) x3 = 0.01, y3 = 0, z3 = 0;
+//    if(isnan(x2) || isnan(y2) || isnan(z2)) x2 = 0.11, y2 = 0, z2 = 0;
+//    if(isnan(x3) || isnan(y3) || isnan(z3)) x3 = 0.01, y3 = 0, z3 = 0;
 
 //    double xp = x + h * a * (y - x);
 //    double yp = y + h * (x * (b - z) - y);
@@ -164,14 +164,14 @@ void drawLeadPointInContext(double xd, double yd, CGContextRef context, CGColor 
     double yp = yprime(h, b, x, y, z);
     double zp = zprime(h, c, x, y, z);
 
-    double x2p = xprime(h, a, x2, y2);
+/*    double x2p = xprime(h, a, x2, y2);
     double y2p = yprime(h, b, x2, y2, z2);
     double z2p = zprime(h, c, x2, y2, z2);
 
     double x3p = xprime(h, a, x3, y3);
     double y3p = yprime(h, b, x3, y3, z3);
     double z3p = zprime(h, c, x3, y3, z3);
-    
+*/
 /*    double xp2 = attractor->xprime(x, y);
     double yp2 = attractor->yprime(x, y, z);
     double zp2 = attractor->zprime(x, y, z);
@@ -180,33 +180,13 @@ void drawLeadPointInContext(double xd, double yd, CGContextRef context, CGColor 
     }*/
 
     x = xp, y = yp, z = zp;
-    x2 = x2p, y2 = y2p, z2 = z2p;
+/*    x2 = x2p, y2 = y2p, z2 = z2p;
     x3 = x3p, y3 = y3p, z3 = z3p;
-    
-    _xyz = [NSString stringWithFormat:@"(%0.2f, %0.2f, %0.2f)\n(%0.2f, %0.2f, %0.2f)\n(%0.2f, %0.2f, %0.2f)", x, y, z, x2, y2, z2, x3, y3, z3];
+*/
+/*    _xyz = [NSString stringWithFormat:@"(%0.2f, %0.2f, %0.2f)\n(%0.2f, %0.2f, %0.2f)\n(%0.2f, %0.2f, %0.2f)", x, y, z , x2, y2, z2, x3, y3, z3];*/
+    _xyz = [NSString stringWithFormat:@"(%0.2f, %0.2f, %0.2f)", x, y, z];
     [xyzCoordinates setText:_xyz];
 
-   
-//    UIColor *newColor = (iterator%2)?[UIColor redColor]:[UIColor blueColor];
-
-/*    if (iterator > 500) {
-        newColor = [UIColor blueColor];
-        
-        //red = ((double)arc4random()/0x100000000);
-        //blue = ((double)arc4random()/0x100000000);
-        //green = ((double)arc4random()/0x100000000);
-        iterator = 0;
-    }*/
-    //UIColor *newColor = [UIColor colorWithRed:zp green:zp blue:zp alpha:0.5];
-//    UIColor *newColor = [UIColor grayColor];
-//    UIColor *newColor = [UIColor colorWithRed:red green:green blue:blue alpha:1.0];
-    //NSLog(@"color: %@", newColor);
-    //CGFloat white = 0.0, alpha = 0.0;
-    
-    //if([newColor getWhite:&white alpha:&alpha]) {
-    //    newColor = [UIColor colorWithWhite:white alpha:0.5];
-    //}
-    
     double xd = (xp*10.0)+self.center.x;
     double yd = (yp*10.0)+self.center.y;
     double zd = 1.0;
@@ -214,16 +194,16 @@ void drawLeadPointInContext(double xd, double yd, CGContextRef context, CGColor 
 //    zd = fabs((zd > 40.0)?fmod(zd, 40.0):zd); zd = (!zd)?1.0:zd;
     
     
-    double x2d = (x2p*10.0)+self.center.x;
+/*    double x2d = (x2p*10.0)+self.center.x;
     double y2d = (y2p*10.0)+self.center.y;
-    double z2d = 1.0;
+    double z2d = 1.0;*/
 //    double z2d = z2p/24.0;
 //    z2d = fabs((z2d > 40.0)?fmod(z2d, 40.0):z2d); z2d = (!z2d)?1.0:z2d;
     
 
-    double x3d = (x3p*10.0)+self.center.x;
+/*    double x3d = (x3p*10.0)+self.center.x;
     double y3d = (y3p*10.0)+self.center.y;
-    double z3d = 1.0;
+    double z3d = 1.0;*/
 //    double z3d = z3p/24.0;
 //    z3d = fabs((z3d > 40.0)?fmod(z3d, 40.0):z3d); z3d = (!z3d)?1.0:z3d;
     
@@ -233,30 +213,8 @@ void drawLeadPointInContext(double xd, double yd, CGContextRef context, CGColor 
         drawLineFromPointToPointInContext(old_xd, old_yd, xd, yd, zd, context, _pointColor.CGColor);
 //        drawLineFromPointToPointInContext(old_x2d, old_y2d, x2d, y2d, z2d, context, _pointColor.CGColor);
 //        drawLineFromPointToPointInContext(old_x3d, old_y3d, x3d, y3d, z3d, context, _pointColor.CGColor);
-        drawLineFromPointToPointInContext(old_x2d, old_y2d, x2d, y2d, z2d, context, [UIColor greenColor].CGColor);
-        drawLineFromPointToPointInContext(old_x3d, old_y3d, x3d, y3d, z3d, context, [UIColor orangeColor].CGColor);
-/*        CGMutablePathRef path = CGPathCreateMutable();
-
-        CGPathMoveToPoint(path, NULL, old_xd, old_yd);
-        CGPathAddLineToPoint(path, NULL, xd, yd);
-        CGContextAddPath(context, path);
-
-        CGContextSetLineWidth(context, zd);
-        CGContextSetStrokeColorWithColor(context, _pointColor.CGColor);
-        CGContextStrokePath(context);
-        CGPathRelease(path);
-        
-        CGMutablePathRef path2 = CGPathCreateMutable();
-        
-        CGPathMoveToPoint(path2, NULL, old_x2d, old_y2d);
-        CGPathAddLineToPoint(path2, NULL, x2d, y2d);
-        CGContextAddPath(context, path2);
-        
-        CGContextSetLineWidth(context, z2d);
-        CGContextSetStrokeColorWithColor(context, [UIColor greenColor].CGColor);
-        CGContextStrokePath(context);
-        CGPathRelease(path2);*/
-
+//        drawLineFromPointToPointInContext(old_x2d, old_y2d, x2d, y2d, z2d, context, [UIColor greenColor].CGColor);
+//        drawLineFromPointToPointInContext(old_x3d, old_y3d, x3d, y3d, z3d, context, [UIColor orangeColor].CGColor);
     } else {
         drawPointsInContext(xd, yd, zd, context, _pointColor.CGColor, _pointColor.CGColor);
 //        drawPointsInContext(x2d, y2d, z2d, context, _pointColor.CGColor, _pointColor.CGColor);
@@ -264,32 +222,17 @@ void drawLeadPointInContext(double xd, double yd, CGContextRef context, CGColor 
 
 //        drawPointsInContext(x2d, y2d, z2d, context, [UIColor greenColor].CGColor, [UIColor greenColor].CGColor);
 //        drawPointsInContext(x3d, y3d, z3d, context, [UIColor orangeColor].CGColor, [UIColor orangeColor].CGColor);
-        
-/*        CGMutablePathRef path = CGPathCreateMutable();
-
-        CGContextSetLineWidth(context, 0.2);
-        
-        CGContextSetFillColorWithColor(context, _pointColor.CGColor);
-        CGPathAddArc(path, NULL, xd, yd, zd, 0, radians(360), 0);
-        CGContextAddPath(context, path);
-
-        CGContextFillPath(context);
-        
-        CGContextSetStrokeColorWithColor(context, _strokeColor.CGColor);
-        CGContextAddPath(context, path);
-        CGContextStrokePath(context);
-        CGPathRelease(path);*/
     }
 
     old_xd = xd;
     old_yd = yd;
     
-    old_x2d = x2d;
+/*    old_x2d = x2d;
     old_y2d = y2d;
 
     old_x3d = x3d;
     old_y3d = y3d;
-
+*/
     NSAssert(context != nil, @"graphics context is nil");
 
     CGImageRef imageRef = CGBitmapContextCreateImage(context);
@@ -301,22 +244,8 @@ void drawLeadPointInContext(double xd, double yd, CGContextRef context, CGColor 
 
     if(_showLeadPoint) {
         drawLeadPointInContext(xd, yd, context, _leadPointColor.CGColor, _leadStrokeColor.CGColor);
-        drawLeadPointInContext(x2d, y2d, context, [UIColor orangeColor].CGColor, [UIColor lightGrayColor].CGColor);
-        drawLeadPointInContext(x3d, y3d, context, [UIColor blueColor].CGColor, [UIColor lightGrayColor].CGColor);
-/*        CGMutablePathRef leadPointPath = CGPathCreateMutable();
-        
-        CGContextSetLineWidth(context, 0.5);
-        CGContextSetFillColorWithColor(context, _leadPointColor.CGColor);
-        CGPathAddArc(leadPointPath, NULL, xd, yd, 10.0, 0, radians(360), 0);
-        CGContextAddPath(context, leadPointPath);
-        CGContextFillPath(context);
-        
-        
-        CGContextSetStrokeColorWithColor(context, _leadStrokeColor.CGColor);
-        CGContextAddPath(context, leadPointPath);
-        CGContextStrokePath(context);
-        
-        CGPathRelease(leadPointPath);*/
+//        drawLeadPointInContext(x2d, y2d, context, [UIColor orangeColor].CGColor, [UIColor lightGrayColor].CGColor);
+//        drawLeadPointInContext(x3d, y3d, context, [UIColor blueColor].CGColor, [UIColor lightGrayColor].CGColor);
     }
 }
 
